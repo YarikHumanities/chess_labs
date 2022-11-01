438cdaed644c467bfc151d5cac837906beac8de4
Yarik <yaroslav.matsapura@gmail.com>
2022-11-01 17:00:59 +0200

    add base game class

---
+++ main.py
@@ -0,0 +1,12 @@
+import chess
+
+class Game:
+    def __init__(self):
+        self.board = chess.Board()
+    
+    def make_move(self):
+        return 0
+    
+    def start(self):
+        return 0
+    
