67fed9857f2618375233d4502c10bae7a8504792
Yarik <yaroslav.matsapura@gmail.com>
2022-11-03 13:45:34 +0200

    negamax implemented

--- algorithms.py
+++ algorithms.py
@@ -1,36 +1,24 @@
 import chess
 
-class Negamax:
-    def __init__(self, board: chess.Board, color: chess.Color, depth: int, heuristic: Heuristic):
-        self.board = board
-        self.color = color
-        self.depth = depth
-        self.heuristic = heuristic
-
-    def negamax(self):
-        if depth == self.depth:
-            return 0
-
-
-
 class heuristic:
     def __init__(self, board: chess.Board):
         self.board = board
-      
 
-    def evaluate(self, color: chess.Color):
+    def evaluate_сell(self, cell: int, color: chess.Color) -> int:
+        score = 0
+        if (self.board.piece_type_at(cell)==color):
+            score=score+1
+        if (self.board.color_at(cell) != color):
+            return score-1
+
+        return score  
+
+    def evaluate(self, color: chess.Color) -> int:
         score = 0
 
         for i in range(64):
-
-            if (self.board.piece_type_at(cell)==color):
-                score = score+1
-
-            if (self.board.color_at(cell) != color):
-                score = score-1
-
-        return score
-
+            score += self.evaluate_сell(chess.SQUARES[i], color)
+       
 
         if (self.board.is_checkmate()):
             if (self.board.turn == color):
@@ -39,3 +27,42 @@ class heuristic:
                 score=score+(9999)
 
         return score
+
+
+class Negamax:
+    def __init__(self, board: chess.Board, color: chess.Color, depth: int, heuristic: heuristic):
+        self.board = board
+        self.color = color
+        self.depth = depth
+        self.heuristic = heuristic
+
+    def get_move(self):
+        max_move = chess.Move.null
+        max_ = float('-inf')
+        for move in self.board.legal_moves:
+            self.board.push(move)
+            score = -self.algorithm(0)
+            self.board.pop()
+
+            if score > max_:
+                max_ = score
+                max_move = move
+
+        return max_move
+
+    def algorithm(self, depth):
+        if depth == self.depth:
+            return self.heuristic.evaluate(self.color)
+
+        max_ = float('-inf')
+        for move in self.board.legal_moves:
+            self.board.push(move)
+            score = -self.algorithm(depth + 1)
+            self.board.pop()
+
+            if score > max_:
+                max_ = score
+
+        return max_
+
+
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import chess
+from algorithms import Negamax, heuristic
 
 class Game:
     def __init__(self):
         self.board = chess.Board()
     
     def make_move(self):
-        return 0
+        print("Making move (before Negamax)")
+        agent = Negamax(self.board, self.board.turn, 1, heuristic(self.board))
+        
+        move = agent.get_move()
+        print("Making move (after Negamax)")
+        self.board.push(move)
     
     def start(self):
-        return 0
+        while not self.board.is_checkmate():
+            print("Will make move")
+            self.make_move()
+            print("Made move")
+            print(self.board)
+            print('\n')
     
+
+game = Game()
+game.start()
